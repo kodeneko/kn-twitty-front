f7984ef341ada4d97fd511c22045d8f0ab1e21d6
Kodeneko <ladysun.freedom@gmail.com>
2025-10-31 13:26:35 +0100

    [dev] base project

---
+++ src/app/layouts/public/public.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-public',
+  imports: [],
+  templateUrl: './public.html',
+  styleUrl: './public.less'
+})
+export class Public {
+
+}
