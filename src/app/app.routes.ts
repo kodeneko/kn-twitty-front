dfb3c7979a459299fa9efe0bcacefa5a0e020557
Kodeneko <ladysun.freedom@gmail.com>
2025-10-31 17:51:40 +0100

    [skeleton] created routing + not found

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,30 @@
 import { Routes } from '@angular/router';
+import { EmptyLayoutComponent } from './layouts/empty/empty-layout.component';
+import { HomePageComponent } from './pages/home/home-page.component';
+import { PublicLayoutComponent } from './layouts/public/public-layout.component';
+import { LoginPageComponent } from './pages/login/login-page.component';
+import { NotFoundPageComponent } from './pages/not-found/not-found-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'public',
+    component: EmptyLayoutComponent,
+    children: [
+      {
+        path: 'login', component: LoginPageComponent
+      }
+    ]
+  },
+  {
+    path: '',
+    component: PublicLayoutComponent,
+    children: [
+      {
+        path: '', component: HomePageComponent
+      },
+      {
+        path: '**', component: NotFoundPageComponent
+      }
+    ]
+  }
+];
