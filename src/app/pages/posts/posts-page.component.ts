09d91f0e5011065ba2738a8edabd690b244c964b
Kodeneko <ladysun.freedom@gmail.com>
2025-11-05 22:20:28 +0100

    [styles] showing results

--- src/app/pages/posts/posts-page.component.ts
+++ src/app/pages/posts/posts-page.component.ts
@@ -1,7 +1,7 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
 import { PostsService } from '../../shared/services/posts.service';
 import { AsyncPipe } from '@angular/common';
-import { catchError, map, of, startWith } from 'rxjs';
+import { catchError, map, Observable, of, startWith, Subscription } from 'rxjs';
 import { ErrorBack } from '../../shared/models/api/error-back.model';
 import { ObservableRes } from '../../shared/models/observable-res.model';
 import { TwitterCountsResponse } from '../../shared/models/twitter/twitter-count-response.model';
@@ -22,8 +22,7 @@ import { ButtonComponent } from "../../shared/components/button/button.component
   templateUrl: './posts-page.component.html',
   styleUrl: './posts-page.component.less'
 })
-export class PostsPageComponent {
-
+export class PostsPageComponent implements OnInit, OnDestroy {
   public faCaretDown = faCaretDown;
   private postService = inject(PostsService);
   public optsDays = [
@@ -31,34 +30,61 @@ export class PostsPageComponent {
     { value: 2, label: "2" },
     { value: 3, label: "3" },
   ];
-
-  countList$: ObservableRes<TwitterCountsResponse> = this.postService.count('cat', 3)
-    .pipe(
-      map(data => {
-        console.log('countList', data);
-        return { data, loading: false, error: null };
-      }),
-      startWith({ data: null, loading: true, error: null }),
-      catchError((error: ErrorBack) => of({ data: null, loading: false, error }))
-    );
-  
-  postList$: ObservableRes<TwitterSearchResponse> = this.postService.list(3)
-    .pipe(
-      map(data => {
-        console.log('postList', data);
-        return { data, loading: false, error: null };
-      }),
-      startWith({ data: null, loading: true, error: null }),
-      catchError((error: ErrorBack) => of({ data: null, loading: false, error }))
-    );
-
-  public user = { 
-    query: 'patata', 
+  public paramsCount = { 
+    query: '', 
     days: 1 
   };
+  public paramsPosts = { 
+    days: 1 
+  };
+  public countList$: ObservableRes<TwitterCountsResponse> | Observable<null> = of(null);
+  public postList$: ObservableRes<TwitterSearchResponse> | Observable<null> = of(null);
+  private countListSubs?: Subscription;
+  private postListSubs?: Subscription;
   
-  onSubmit(form: NgForm) { 
+  ngOnInit(): void {
+    this.getCount(this.paramsCount.query, this.paramsCount.days);
+    this.getPosts(this.paramsPosts.days);
+  }
+
+  ngOnDestroy(): void {
+    this.countListSubs?.unsubscribe(); 
+    this.postListSubs?.unsubscribe();
+  }
+
+  public getCount(query: string, days: number) {
+    this.countList$ = this.postService.count(query, days)
+      .pipe(
+        map(data => {
+          console.log('countList', data);
+          return { data, loading: false, error: null };
+        }),
+        startWith({ data: null, loading: true, error: null }),
+        catchError((error: ErrorBack) => of({ data: null, loading: false, error }))
+      );
+  }
+
+  public getPosts(days: number) {
+    this.postList$ = this.postService.list(days)
+      .pipe(
+        map(data => {
+          console.log('postList', data);
+          return { data, loading: false, error: null };
+        }),
+        startWith({ data: null, loading: true, error: null }),
+        catchError((error: ErrorBack) => of({ data: null, loading: false, error }))
+      );
+  }
+  
+  public onSubmitCount(form: NgForm) {
     console.log('Form:', form.value);
-    console.log('Form:', this.user);
+    console.log('Form:', this.paramsCount);
+    this.getCount(form.value.query, form.value.days);
+  }
+
+  public onSubmitPosts(form: NgForm) {
+    console.log('Form:', form.value);
+    console.log('Form:', this.paramsPosts);
+    this.getPosts(form.value.days);
   }
 }
