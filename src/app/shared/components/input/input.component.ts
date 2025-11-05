26f9e40d04cd39b25f917b736a9621ff37ef0517
Kodeneko <ladysun.freedom@gmail.com>
2025-11-05 19:38:49 +0100

    [style] custom forms

--- src/app/shared/components/input/input.component.ts
+++ src/app/shared/components/input/input.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-empty-function */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { Component, forwardRef } from '@angular/core';
+import { NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
   selector: 'app-input',
+  providers: [{
+    provide: NG_VALUE_ACCESSOR,
+    useExisting: forwardRef(() => InputComponent),
+    multi: true
+  }],
   imports: [],
   templateUrl: './input.component.html',
   styleUrl: './input.component.less'
 })
 export class InputComponent {
+  value = '';
+  onChange = (value: any) => {};
+  onTouched = () => {};
+
+  onInput(value: string) {
+    this.value = value;
+    this.onChange(value);
+  }
+
+  // Método del ControlValueAccessor
+  writeValue(value: any): void {
+    this.value = value || '';
+  }
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+  setDisabledState?(isDisabled: boolean): void {
+    // Maneja el estado deshabilitado si lo necesitas
+  }
 }
--- src/app/shared/components/select/select.component.ts
+++ src/app/shared/components/select/select.component.ts
@@ -1,13 +1,47 @@
-import { Component } from '@angular/core';
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-empty-function */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { Component, forwardRef, input } from '@angular/core';
 import { faCaretDown } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { NG_VALUE_ACCESSOR, NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-select',
   imports: [FontAwesomeModule],
+  providers: [{
+    provide: NG_VALUE_ACCESSOR,
+    useExisting: forwardRef(() => SelectComponent),
+    multi: true
+  }],
   templateUrl: './select.component.html',
   styleUrl: './select.component.less'
 })
 export class SelectComponent {
   public faCaretDown = faCaretDown;
+  label = input<string>('');
+  options = input<{ value: any, label: string }[]>([]);
+
+  value: any;
+  onChange = (value: any) => {};
+  onTouched = () => {};
+
+  onSelect(event: any) {
+    const value = event.target.value;
+    this.value = value;
+    this.onChange(value);
+  }
+
+  writeValue(value: any): void {
+    this.value = value;
+  }
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+  setDisabledState?(isDisabled: boolean): void {
+    // Implement if needed
+  }
 }
